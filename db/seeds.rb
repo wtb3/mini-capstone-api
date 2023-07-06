5b8c9411b951785316fc407388298320540beeaf
Warren Barnes <warrenthomasbarnes@gmail.com>
2023-07-06 13:22:30 -0400

    Add seed data for products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Product.create(name: "Product 1", price: 10, image_url: "test.com", description: "Description 1")
+Product.create(name: "Product 2", price: 20, image_url: "test.com", description: "Description 2")
